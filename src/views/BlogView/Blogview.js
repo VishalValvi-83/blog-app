a5eb9a3e0e6841ae232a5d65148bd049fad28dd9
VishalValvi-83 <vishalvalvi2529@gmail.com>
2024-06-23 12:16:47 +0530

    Changes added

--- src/views/BlogView/Blogview.js
+++ src/views/BlogView/Blogview.js
@@ -17,18 +17,18 @@ function Blogview() {
   return (
     <div className="container">
       <div className="blog-detailed-Container">
-        <div class="blog-header">
-          <div class="title-area clearfix">
-            <span class="detail-publish-author">By: <strong>{selectedBlog.author.name}</strong></span>
+        <div className="blog-header">
+          <div className="title-area clearfix">
+            <span className="detail-publish-author">By: <strong>{selectedBlog.author.name}</strong></span>
           </div>
           <h1>{selectedBlog.title}</h1>
-          <div class="share-n-tag-wrap clearfix">
-            <figure class="blog-img">
+          <div className="share-n-tag-wrap clearfix">
+            <figure className="blog-img">
               <img src={selectedBlog.image} alt={selectedBlog.title} />
             </figure>
           </div>
         </div>
-        <div class="text parbase">
+        <div className="text parbase">
           <p>{selectedBlog.content}</p>
           <p>{selectedBlog.detail.para1}</p>
           <p>{selectedBlog.detail.para2}</p>
@@ -37,25 +37,25 @@ function Blogview() {
           <p>{selectedBlog.detail.para5}</p>
         </div>
         <div>
-          <div class="tag-outer-wrap">
-            <a href="/content/sih/en/bloglist.html?tag=india" class="tag blue">India</a>
+          <div className="tag-outer-wrap">
+            <a href="/" className="tag blue">Home Page</a>
           </div>
         </div>
         <div className="bottom-share-wrap">
           <strong> Share: </strong>
-          <ul class="bottom-share-list" >
-            <li class="share-link">
-              <a class="facebook" href="/" >
+          <ul className="bottom-share-list" >
+            <li className="share-link">
+              <a className="facebook" href="https://www.facebook.com" >
                 <img  src={FacebookIcon} alt="facebook" title="facebook" />
               </a>
             </li>
-            <li class="share-link">
-              <a class="linkedin" href="/" >
+            <li className="share-link">
+              <a className="linkedin" href="https://www.linkedin.com/" >
                 <img  src={LinkedInIcon} alt="linkedin" title="linkedin" />
               </a>
             </li>
-            <li class="share-link">
-              <a class="twitter" href="/" >
+            <li className="share-link">
+              <a className="twitter" href="https://x.com/" >
                 <img  src={TwitterIcon} alt="twitter" title="twitter" />
               </a>
             </li>
